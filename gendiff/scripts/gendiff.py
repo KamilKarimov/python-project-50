28ffff048632b085458ce478f5337a83b402082b
Камиль Каримов <chitatilo@gmail.com>
2022-12-05 13:27:50 +0400

    fix gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,9 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(prog='gendiff',
+                                     usage='gendiff [-h] [-f FORMAT] first_file second_file',
+                                     description='Compares two configuration files and shows a difference.')
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
