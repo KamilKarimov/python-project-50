cf9d50b0e407b254c242539f27b62f5432432eb1
Камиль Каримов <chitatilo@gmail.com>
2022-12-09 18:38:41 +0400

    fix pytest errors

--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -3,7 +3,7 @@ from gendiff.modules.gendiff import generate_diff
 
 
 def test_generate_diff():
-    f1 = json.load(open(first_file))
-    f2 = json.load(open(second_file))
+    f1 = json.load(open('tests/fixtures/file1.json'))
+    f2 = json.load(open('tests/fixtures/file2.json'))
     with open('tests/fixtures/correct_diff', mode='r') as f:
         assert generate_diff(f1, f2) == f.read()
